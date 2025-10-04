ba81342b79fbdf0b09baaf5360e1ab00a264dc0c
trm <956303669@qq.com>
2025-10-04 20:06:40 +0800

    定义测试用例

---
+++ tests/create_container_test.py
@@ -0,0 +1,12 @@
+#TODO: 完成测试用例编写
+import pytest
+
+def test_happy_path():
+    raise NotImplementedError
+
+def test_error_path_invalid_config():
+    raise NotImplementedError
+
+def test_error_path_container_exist():
+    raise NotImplementedError
+
---
+++ tests/update_role_test.py
@@ -0,0 +1,20 @@
+#TODO: 完成测试用例编写
+import pytest
+
+def test_happy_path():
+    raise NotImplementedError
+
+def test_error_path_invalid_config():
+    raise NotImplementedError
+
+def test_error_path_collaborator_not_exist():
+    raise NotImplementedError
+
+def test_error_path_the_same_role():
+    raise NotImplementedError
+
+def test_error_path_container_not_exist():
+    raise NotImplementedError
+
+def test_error_path_role_are_root():
+    raise NotImplementedError
