48f0c5408e87a70f1b25f625dd3e00e419a9a1f3
trm <956303669@qq.com>
2025-11-02 21:29:58 +0800

    fix

--- run.py
+++ run.py
@@ -1,10 +1,10 @@
 import sys
 import os
 
-# 将项目根目录添加到 Python 路径
-sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
+# 将父目录添加到 Python 路径
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from __init__ import create_app
+from FuxiYu_NodeKernel import create_app
 
 if __name__ == '__main__':
     app = create_app('development')
