f0fe37241b9b6affd745cb4752b455149ae43668
Chester W. <survivor_wqw133@163.com>
2026-02-06 02:34:04 +0000

    - 小的临时修改 为迎合Ctrl与Node的port占用情况，Node的port：5000->5001 - 补充了requirements.txt的缺项

--- run.py
+++ run.py
@@ -8,5 +8,5 @@ from FuxiYu_NodeKernel import create_app
 
 if __name__ == '__main__':
     app = create_app('development')
-    app.run(host = '0.0.0.0', debug=True)
+    app.run(host = '0.0.0.0', port=5001, debug=True)
 
