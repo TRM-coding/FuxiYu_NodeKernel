19387247f0ab07c86816d79482fbd0e59df01380
chester <survivor_wqw133@163.com>
2026-08-14 13:33:19 +0800

    - PREP 继续开发前的修补工作——  弃用了flask脚手架的无意义内容

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from .extensions import db, migrate, cache, login_manager, init_docker
+from .extensions import init_docker
 from .config import get_config
 from .blueprints import register_blueprints
 from .utils.logging_config import configure_daily_logging
@@ -9,11 +9,6 @@ def create_app(config: str | None = None):
     app.config.from_object(get_config(config))
     configure_daily_logging(app)
 
-    db.init_app(app)
-    migrate.init_app(app, db)
-    cache.init_app(app)
-    login_manager.init_app(app)
-    # init_docker()
 
     register_blueprints(app)
     return app
--- extensions.py
+++ extensions.py
@@ -1,15 +1,6 @@
-# extensions.py
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-from flask_caching import Cache
-from flask_login import LoginManager
 import docker
 docker_client=None
 
-db = SQLAlchemy()
-migrate = Migrate()
-cache = Cache()
-login_manager = LoginManager()
 
 def init_docker():
     global docker_client
